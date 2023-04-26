e6a6c2e217d44ac62f9fa9abf7c12de3833942f5
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2023-04-26 16:27:10 -0300

    refactor structure files

---
+++ jogo_adivinhacao/game.rb
@@ -0,0 +1,97 @@
+require_relative 'ui.rb'
+
+def calculate_points(points, input_number, secret_number)
+    points -= (input_number - secret_number).abs * 0.5
+    points
+end
+
+def generate_secret_number(max_attempts)
+    return rand(max_attempts)
+end
+
+def available_shot(number, secret, attempt)
+    correct = number == secret
+    if !correct
+        bigger = number > secret
+        print_invalid_shot(bigger)
+        return false
+    end
+    return true
+end
+
+def get_difficulties()
+    return [
+        { "MUITO FÁCIL": 30 }, 
+        { "FÁCIL": 60 }, 
+        { "MÉDIO": 100 }, 
+        { "DIFÍCIL": 150 }, 
+        { "MUITO DIFÍCIL": 200 } 
+    ]
+end
+
+def available_range_of_shot_number(input_number, max_numbers, shots)
+
+    loop do
+        if !(input_number.strip !~ /\D/) || !input_number.to_i.between?(0, max_numbers)
+            print_out_of_range_shot(max_numbers)
+            input_number = get_shot_number(max_numbers)
+            next
+        end
+        break
+    end
+
+end
+
+def available_is_repeated_shot(input_number, max_numbers, shots)
+
+    loop do
+        if shots.include?(input_number.to_i)
+            print_repeated_shot()
+            input_number = get_shot_number(max_numbers)
+            next
+        end
+        break
+    end
+
+end
+
+def play()
+
+    print_intro()
+    name = get_player_name()
+    
+    playing = true
+    
+    while playing
+
+        max_attempts = 5 
+        difficulties = get_difficulties()
+        print_difficulties(difficulties)
+        max_numbers = ask_difficulty(difficulties)
+        secret_number = generate_secret_number(max_numbers)
+        
+        points = 1000
+        shots = []
+        attempts = 1
+        hit = false
+
+        while attempts <= max_attempts && hit == false
+
+            print_trying(attempts, max_attempts, shots)
+            input_number = get_shot_number(max_numbers)
+            available_range_of_shot_number(input_number, max_numbers, shots)
+            available_is_repeated_shot(input_number, max_numbers, shots)
+            input_number = print_shot_number(input_number.to_i)
+            points = calculate_points(points, input_number, secret_number)
+            shots << input_number
+            hit = available_shot(input_number, secret_number, attempts)
+            attempts += 1
+
+        end
+        
+        print_win_and_points_or_lose(hit, points, secret_number, attempts-1)
+        playing = available_play_again()
+
+    end 
+
+end
---
+++ jogo_adivinhacao/ui.rb
@@ -0,0 +1,120 @@
+def print_intro()
+    puts
+    puts "        P  /_\\  P                              "
+    puts "       /_\\_|_|_/_\\                             "
+    puts "   n_n | ||. .|| | n_n         Bem vindo ao    "
+    puts "   |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!"
+    puts "  |' '  |  |_|  |'  ' |                        "
+    puts "  |_____| ' _ ' |_____|                        " 
+    puts "        \\__|_|__/                              "
+    puts "\n\n"
+end
+
+def get_player_name()
+    puts "Informe seu nome: "
+    name = gets.strip
+    puts "Olá #{name.strip}! Vamos iniciar então...\n\n"
+    name
+end
+
+def print_difficulties(difficulties)
+    puts "**********************************************"
+    puts "DIFICULDADES"
+    puts "**********************************************\n\n"
+    difficulties.each_with_index do |child, index| 
+        child.each do |key, value| 
+            puts "#{index + 1} - #{key} - de 0 a #{value}" 
+        end
+    end
+end
+
+def ask_difficulty(difficulties)
+
+    puts "\n**********************************************\n\n"
+    puts "Informe em qual nível de dificuldade deseja jogar (de 1 a 5): "
+    difficulty = gets 
+    while !(difficulty.strip !~ /\D/) || !difficulty.to_i.between?(1, 5)
+        puts "A dificuldade do jogo precisa ser um número entre 1 e 5. Informe novamente: "
+        difficulty = gets
+    end
+    puts "\n"
+    print_chosen_difficulty(difficulties, difficulty)
+end
+
+def print_chosen_difficulty(difficulties, difficulty)
+    difficulties[difficulty.to_i - 1].each { 
+        |key, value| 
+        puts "**********************************************\n"
+        puts "Você escolheu a dificuldade #{key} de 0 a #{value}."
+        return value
+    }
+end
+
+def print_out_of_range_shot(max_numbers)
+    puts "Seu chute precisa ser um número entre 0 e #{max_numbers}."
+end
+
+def print_repeated_shot()
+    puts "Você já chutou esse número."
+end
+
+def get_shot_number(max_numbers)
+    puts "Dê o seu chute entre 0 e #{max_numbers}: "
+    return gets.strip
+end
+
+def print_shot_number(input_number) 
+    puts "\n"
+    puts "Você chutou #{input_number}... Será que você acertou? "
+    return input_number
+end
+
+def print_invalid_shot(bigger)
+    puts "Não foi dessa vez :( - O número secreto é #{bigger ? "MENOR" : "MAIOR"} que o seu chute."
+end
+
+def print_trying(sequence, max_attempts, shots)    
+    puts "\n"
+    if shots.any? # if shots.size > 0
+        puts "Tentativa #{sequence} de #{max_attempts} - Chutes anteriores #{shots}."
+    else
+        puts "Tentativa #{sequence} de #{max_attempts}"
+    end
+end
+
+def print_win_and_points_or_lose(hit, points, secret_number, attempt) 
+    puts "\n"
+    puts "**********************************************"
+    puts "\n"
+    if !hit 
+        puts "HUUMMMM! Infelizmente suas chances acabaram! O número secreto era #{secret_number}. Tente novamente?"
+    else
+        puts "Parabéns!! Você acertou na #{attempt}a tentativa!!"
+        puts
+        puts "             OOOOOOOOOOO               "
+        puts "         OOOOOOOOOOOOOOOOOOO           "
+        puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
+        puts "    OOOOOO      OOOOO      OOOOOO      "
+        puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
+        puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
+        puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+        puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+        puts "OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  "
+        puts " OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   "
+        puts "  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    "
+        puts "    OOOOO   OOOOOOOOOOOOOOO   OOOO     "
+        puts "      OOOOOO   OOOOOOOOO   OOOOOO      "
+        puts "         OOOOOO         OOOOOO         "
+        puts "             OOOOOOOOOOOO              "
+        puts
+        puts "Você ganhou #{points} pontos nessa rodada."
+    end
+end
+
+def available_play_again()
+    puts "\n"
+    puts "**********************************************"
+    puts "\n"
+    puts "Informe S ou s para jogar novamente ou qualquer outra tecla para encerrar: "
+    return gets.strip.upcase == "S"
+end
